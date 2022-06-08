a4bc43d12cc47fd0b0d4a775ed13ae463fa5d893
Fabrizio Carlassara <53102151+carlassaraf@users.noreply.github.com>
2022-06-07 21:53:26 -0300

    added chip libraries

---
+++ chip/lpc_chip_43xx/Debug/src/fpu_init.d
@@ -0,0 +1,25 @@
+src/fpu_init.o src/fpu_init.d: ../src/fpu_init.c \
+ D:\Facultad\Quinto\TD\ III\workspace\lpc_chip_43xx\inc\config_43xx/sys_config.h \
+ D:\Facultad\Quinto\TD\ III\workspace\lpc_chip_43xx\inc/cmsis.h \
+ D:\Facultad\Quinto\TD\ III\workspace\lpc_chip_43xx\inc/lpc_types.h \
+ D:\Facultad\Quinto\TD\ III\workspace\lpc_chip_43xx\inc/cmsis_43xx.h \
+ D:\Facultad\Quinto\TD\ III\workspace\lpc_chip_43xx\inc/core_cm4.h \
+ D:\Facultad\Quinto\TD\ III\workspace\lpc_chip_43xx\inc/core_cmInstr.h \
+ D:\Facultad\Quinto\TD\ III\workspace\lpc_chip_43xx\inc/core_cmFunc.h \
+ D:\Facultad\Quinto\TD\ III\workspace\lpc_chip_43xx\inc/core_cm4_simd.h
+
+D:\Facultad\Quinto\TD\ III\workspace\lpc_chip_43xx\inc\config_43xx/sys_config.h:
+
+D:\Facultad\Quinto\TD\ III\workspace\lpc_chip_43xx\inc/cmsis.h:
+
+D:\Facultad\Quinto\TD\ III\workspace\lpc_chip_43xx\inc/lpc_types.h:
+
+D:\Facultad\Quinto\TD\ III\workspace\lpc_chip_43xx\inc/cmsis_43xx.h:
+
+D:\Facultad\Quinto\TD\ III\workspace\lpc_chip_43xx\inc/core_cm4.h:
+
+D:\Facultad\Quinto\TD\ III\workspace\lpc_chip_43xx\inc/core_cmInstr.h:
+
+D:\Facultad\Quinto\TD\ III\workspace\lpc_chip_43xx\inc/core_cmFunc.h:
+
+D:\Facultad\Quinto\TD\ III\workspace\lpc_chip_43xx\inc/core_cm4_simd.h:
