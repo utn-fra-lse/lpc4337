884688cf3d587f01b4f10a06b345c6d2dc3e22cf
Fabrizio Carlassara <53102151+carlassaraf@users.noreply.github.com>
2022-06-10 01:13:53 -0300

    add support for uart

---
+++ api/inc/ciaa_uart_api.h
@@ -0,0 +1,116 @@
+/*
+ * ciaa_uart_api.h
+ *
+ *  Created on: Jun 9, 2022
+ *      Author: fabri
+ */
+
+#ifndef CIAA_UART_API_H_
+#define CIAA_UART_API_H_
+
+#include "chip.h"
+#include "ciaa_scu_api.h"
+#include "ciaa_gpio_api.h"
+
+/* USART base register definitions */
+#define CIAA_UART0	LPC_USART0
+#define CIAA_UART1	LPC_UART1
+#define CIAA_UART2	LPC_USART2
+#define CIAA_UART3	LPC_USART3
+
+/* UART definitions */
+typedef enum {
+	UART_0,
+	UART_1,
+	UART_2,
+	UART_3
+} ciaa_uart_t;
+
+/* UART line status definitions */
+typedef enum {
+	UART_RECEIVE_DATA_READY = (1 << 0),		/* Line status: Receive data ready */
+	UART_OVERRUN_ERROR	= (1 << 1),			/* Line status: Overrun error */
+	UART_PARITY_ERROR = (1 << 2),			/* Line status: Parity error */
+	UART_FRAMING_ERROR = (1 << 3),			/* Line status: Framing error */
+	UART_BREAK_INTERRUPT = (1 << 4),		/* Line status: Break interrupt */
+	UART_HOLDING_REG_EMPTY = (1 << 5),		/* Line status: Transmit holding register empty */
+	UART_TX_EMPTY = (1 << 6),				/* Line status: Transmitter empty */
+	UART_RX_FIFO_ERROR = (1 << 7),			/* Line status: Error in RX FIFO */
+	UART_TX_FIFO_ERROR = (1 << 8)			/* Line status: Error in RX FIFO */
+} uart_line_status_t;
+
+/* UART Stop bit select bit definitions */
+typedef enum {
+	UART_STOP_BITS_1 = (0 << 2),			/* UART stop bit select: 1 stop bit */
+	UART_STOP_BITS_2 = (1 << 2)    			/* UART stop bit select: 2 stop bits (in 5 bit data mode, 1.5 stop bits) */
+} uart_stop_bits_t;
+
+/* UART Parity enable bit definitions */
+typedef enum {
+	UART_PARITY_ENABLE = (1 << 3),			/* UART Parity Enable */
+	UART_PARITY_DISABLE = (0 << 3),			/* UART Parity Disable */
+	UART_PARITY_ODD = (0 << 4),				/* UART Parity select: Odd parity */
+	UART_PARITY_EVEN = (1 << 4),			/* UART Parity select: Even parity */
+	UART_STICK_1 = (2 << 4),				/* UART Parity select: Forced 1 stick parity */
+	UART_STICK_2 = (3 << 4)					/* UART Parity select: Forced 0 stick parity */
+} uart_parity_t;
+
+/* UART word length select bit definitions */
+typedef enum {
+	UART_WORD_LENGTH_5 = (0 << 0),			/* UART word length select: 5 bit data mode */
+	UART_WORD_LENGTH_6 = (1 << 0),			/* UART word length select: 6 bit data mode */
+	UART_WORD_LENGTH_7 = (2 << 0),			/* UART word length select: 7 bit data mode */
+	UART_WORD_LENGTH_8 = (3 << 0)			/* UART word length select: 8 bit data mode */
+} uart_word_length_t;
+
+typedef struct {
+	ciaa_uart_t uart;
+	uint32_t baudRate;
+	uart_stop_bits_t stopBits;
+	uart_parity_t parityBits;
+	uart_word_length_t wordLength;
+	pin_t *txPin;
+	pin_t *rxPin;
+} uart_config_t;
+
+/* Extern UART base register array */
+extern LPC_USART_T *CIAA_UARTS[];
+/* Extern UART default configurations */
+extern uart_config_t UART_DEFAULT_CONFIGS[];
+
+/* Function prototypes */
+void uart_init(ciaa_uart_t uart);
+void uart_config_init(uart_config_t config);
+void uart_puts(ciaa_uart_t usart, const char* str);
+void uart_set_baud_rate(ciaa_uart_t usart, uint32_t baudRate);
+
+/* Inline functions */
+
+static inline LPC_USART_T* uart_get_base_register(ciaa_uart_t uart) { return CIAA_UARTS[uart]; }
+
+static inline uint8_t uart_get_config_value(uart_config_t config) { return (config.stopBits | config.parityBits | config.wordLength); }
+
+static inline void uart_set_config_value(uart_config_t config) { uart_get_base_register(config.uart)->LCR = (config.stopBits | config.parityBits | config.wordLength); }
+
+static inline uart_config_t uart_get_default_config(ciaa_uart_t uart) { return UART_DEFAULT_CONFIGS[uart]; }
+
+static inline void uart_setup_fifos(ciaa_uart_t uart) { uart_get_base_register(uart)->FCR = (UART_FCR_FIFO_EN | UART_FCR_RX_RS | UART_FCR_TX_RS); }
+
+static inline void uart_set_tx_pin(ciaa_uart_t uart, pin_t *pin) { scu_set_pin_mode(pin->SCU_PORT, pin->SCU_PIN, (SCU_MODE_INACT | pin->FUNCTIONS->USART)); }
+
+static inline void uart_set_rx_pin(ciaa_uart_t uart, pin_t *pin) { scu_set_pin_mode(pin->SCU_PORT, pin->SCU_PIN, (SCU_MODE_INACT | SCU_MODE_INBUFF_EN | pin->FUNCTIONS->USART)); }
+
+static inline void uart_tx_enable(ciaa_uart_t uart) { uart_get_base_register(uart)->TER2 = UART_TER2_TXEN; }
+
+static inline void uart_tx_disable(ciaa_uart_t uart) { uart_get_base_register(uart)->TER2 = 0; }
+
+static inline void uart_set_tx_enabled(ciaa_uart_t uart, bool enabled) {
+	uart_tx_disable(uart);
+	if(enabled) { uart_tx_enable(uart); }
+}
+
+static inline uart_line_status_t uart_get_line_status(ciaa_uart_t uart) { return (uart_line_status_t) uart_get_base_register(uart)->LSR; };
+
+static inline void uart_putc(ciaa_uart_t uart, char data) { uart_get_base_register(uart)->THR = (uint32_t) data; }
+
+#endif /* CIAA_UART_API_H_ */
---
+++ api/src/ciaa_uart_api.c
@@ -0,0 +1,98 @@
+/*
+ * ciaa_uart_api.c
+ *
+ *  Created on: Jun 9, 2022
+ *      Author: fabri
+ */
+
+#include <ciaa_uart_api.h>
+
+LPC_USART_T *CIAA_UARTS[] = { CIAA_UART0, CIAA_UART1, CIAA_UART2, CIAA_UART3 };
+
+static const CHIP_CCU_CLK_T CIAA_UART_CLKS[] = { CLK_MX_UART0, CLK_MX_UART1, CLK_MX_UART2, CLK_MX_UART3 };
+
+uart_config_t UART_DEFAULT_CONFIGS[] = {
+	{ UART_0, 115200, UART_STOP_BITS_1, UART_PARITY_DISABLE, UART_WORD_LENGTH_8, &GPIO1, &GPIO2 },
+	{ UART_1, 115200, UART_STOP_BITS_1, UART_PARITY_DISABLE, UART_WORD_LENGTH_8, NULL, NULL },
+	{ UART_2, 115200, UART_STOP_BITS_1, UART_PARITY_DISABLE, UART_WORD_LENGTH_8, &RXD0, &CRS_DV },
+	{ UART_3, 115200, UART_STOP_BITS_1, UART_PARITY_DISABLE, UART_WORD_LENGTH_8, &RS485_TXD, &RS485_RXD }
+};
+
+void uart_init(ciaa_uart_t uart) {
+	/* Get default UART configuration and initialize */
+	uart_config_init(uart_get_default_config(uart));
+}
+
+void uart_config_init(uart_config_t config) {
+	/* Get base register */
+	LPC_USART_T *reg = uart_get_base_register(config.uart);
+	/* Select TX pin */
+	uart_set_tx_pin(config.uart, config.txPin);
+	/* Select RX pin */
+	uart_set_rx_pin(config.uart, config.rxPin);
+	/* Enable UART clocking. UART base clock(s) must already be enabled */
+	Chip_Clock_EnableOpts(CIAA_UART_CLKS[config.uart], true, true, 1);
+	/* Enable FIFOs by default, reset them */
+	uart_setup_fifos(config.uart);
+    /* Disable Tx pin */
+	uart_set_tx_enabled(config.uart, false);
+    /* Disable interrupts */
+	reg->IER = 0;
+	/* Set LCR to default state */
+	reg->LCR = 0;
+	/* Set ACR to default state */
+	reg->ACR = 0;
+    /* Set RS485 control to default state */
+	reg->RS485CTRL = 0;
+	/* Set RS485 delay timer to default state */
+	reg->RS485DLY = 0;
+	/* Set RS485 addr match to default state */
+	reg->RS485ADRMATCH = 0;
+    /* Clear MCR */
+    if (reg == CIAA_UART1) {
+		/* Set Modem Control to default state */
+		reg->MCR = 0;
+		/* Configure USART */
+	    volatile uint32_t tmp;
+		/*Dummy Reading to Clear Status */
+		tmp = reg->MSR;
+	}
+	/* Default 8N1, with DLAB disabled */
+    uart_set_config_value(config);
+	/* Disable fractional divider */
+	reg->FDR = 0x10;
+	/* Set USART baud rate */
+	uart_set_baud_rate(config.uart, config.baudRate);
+	/* Enable TX pin */
+	uart_set_tx_enabled(config.uart, true);
+}
+
+void uart_set_baud_rate(ciaa_uart_t usart, uint32_t baudRate) {
+	/* Get USART base register */
+	LPC_USART_T *reg = uart_get_base_register(usart);
+	/* Clock divider variables */
+	uint32_t div, divh, divl, clkin;
+	/* Determine UART clock in rate without FDR */
+	clkin = Chip_Clock_GetRate(CIAA_UART_CLKS[usart]);
+	div = clkin / (baudRate * 16);
+	/* High and low halves of the divider */
+	divh = div / 256;
+	divl = div - (divh * 256);
+	/* Enable divisor access */
+	reg->LCR |= UART_LCR_DLAB_EN;
+	/* Set divisor latches */
+	reg->DLL = divl;
+	reg->DLM = divh;
+	/* Disable divisor access */
+	reg->LCR &= ~UART_LCR_DLAB_EN;
+}
+
+void uart_puts(ciaa_uart_t uart, const char* str) {
+	/* Loop until null character */
+	while(*str) {
+		/* Wait until the transmit holding register is empty */
+		while ( !(uart_get_line_status(uart) & UART_HOLDING_REG_EMPTY) );
+		/* Send byte */
+		uart_putc(uart, *str++);
+	}
+}
