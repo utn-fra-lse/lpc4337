ae64c50547cbec4c6749bc553b35ee83f8fc2edc
Fabrizio Carlassara <53102151+carlassaraf@users.noreply.github.com>
2022-06-11 00:32:33 -0300

    added some comments

--- examples/uart/uart_echo/src/uart_echo.c
+++ examples/uart/uart_echo/src/uart_echo.c
@@ -11,16 +11,17 @@
 #include "ciaa_uart_api.h"
 
 int main(void) {
-    // Read clock settings and update SystemCoreClock variable
+    /* Variable to store string */
+    char str[50];
+    /* Read clock settings and update SystemCoreClock variable */
     SystemCoreClockUpdate();
-
+    /* Initialize UART0 with default configuration */
     uart_init(UART_0);
 
-    char str[50];
-
     while(1) {
-
+    	/* Read string from UART */
     	uart_gets(UART_0, str);
+    	/* Write string back */
     	uart_puts(UART_0, str);
     }
     return 0 ;
