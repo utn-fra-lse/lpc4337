dcf081052898f8b1834f6e88ebbe0664c5b8b7a1
Fabrizio Carlassara <53102151+carlassaraf@users.noreply.github.com>
2022-06-14 11:30:30 -0300

    add lcd 16x2 support

--- api/inc/ciaa_lcd_api.h
+++ api/inc/ciaa_lcd_api.h
@@ -12,22 +12,25 @@
 #include "ciaa_gpio_api.h"
 #include "ciaa_stdlib.h"
 
+/* LCD struct with pointers to pins */
 typedef struct {
-	pin_t *en;
-	pin_t *rs;
-	pin_t *d4;
-	pin_t *d5;
-	pin_t *d6;
-	pin_t *d7;
+	pin_t *en;		/* Enable pin pointer */
+	pin_t *rs;		/* Register Select pin pointer */
+	pin_t *d4;		/* Data pin 4 pointer */
+	pin_t *d5;		/* Data pin 5 pointer */
+	pin_t *d6;		/* Data pin 6 pointer */
+	pin_t *d7;		/* Data pin 7 pointer */
 } lcd_t;
 
+/* LCD configuration struct */
 typedef struct {
-	lcd_t lcd;
-	bool cursor;
-	bool blink;
-	bool bigFont;
+	lcd_t lcd;		/* Pin struct */
+	bool cursor;	/* Cursor enabled or not */
+	bool blink;		/* Blink cursor or not */
+	bool bigFont;	/* 5x10 or 5x7 font */
 } lcd_config_t;
 
+/* Function prototypes */
 void lcd_init(void);
 void lcd_config_init(lcd_config_t lcd);
 void lcd_put_nibble(lcd_t lcd, uint8_t nibble);
@@ -37,16 +40,32 @@ void lcd_puts(lcd_t lcd, const char* str);
 void lcd_clear(lcd_t lcd);
 void lcd_go_to_xy(lcd_t lcd, uint8_t x,  uint8_t y);
 
+/* Inline functions */
+
+/*
+ * 	@brief	Get LCD default configuration
+ *
+ * 	@prarm	None
+ *
+ * 	@return	LCD default configuration struct
+ */
 static inline lcd_config_t lcd_get_default_config(void) {
 	lcd_config_t config = {
-		{ &LCD_EN, &LCD_RS, &LCD1, &LCD2, &LCD3, &LCD4 },
-		false,
-		false,
-		false
+		{ &LCD_EN, &LCD_RS, &LCD1, &LCD2, &LCD3, &LCD4 },	/* LCD default pins */
+		false,												/* No cursor */
+		false,												/* No blink */
+		false												/* 5x7 font */
 	};
 	return config;
 }
 
+/*
+ * 	@brief	Get LCD pins from config struct
+ *
+ * 	@param	config: LCD configuration struct
+ *
+ * 	@param	return: LCD pin struct
+ */
 static inline lcd_t lcd_get_from_config(lcd_config_t config) {
 	return config.lcd;
 }
--- api/src/ciaa_lcd_api.c
+++ api/src/ciaa_lcd_api.c
@@ -7,10 +7,25 @@
 
 #include "ciaa_lcd_api.h"
 
+/*
+ * 	@brief	Initialize LCD with default configuration
+ *
+ * 	@param	None
+ *
+ * 	@return	None
+ */
 void lcd_init(void) {
+	/* Get LCD default config and initialize */
 	lcd_config_init(lcd_get_default_config());
 }
 
+/*
+ * 	@brief	Initialize LCD with given configuration
+ *
+ * 	@param	config: LCD configuration
+ *
+ * 	@return	None
+ */
 void lcd_config_init(lcd_config_t config) {
 	/* Get LCD struct */
 	lcd_t lcd = lcd_get_from_config(config);
@@ -54,10 +69,17 @@ void lcd_config_init(lcd_config_t config) {
   	lcd_put_command(lcd, 0x06);
 }
 
+/*
+ * 	@brief	Write a new nibble in data pins
+ *
+ * 	@param	lcd: LCD pin struct
+ * 	@param	nibble: four bit data
+ *
+ * 	@return	None
+ */
 void lcd_put(lcd_t lcd, uint8_t nibble) {
-
+	/* Get pointer to enable pin */
 	pin_t *en = lcd.en;
-
 	/* Get pointer to the pointer of the first pin in struct */
 	pin_t **p = &(lcd.d4);
 	/* Get the number of elements to loop through */
@@ -71,56 +93,111 @@ void lcd_put(lcd_t lcd, uint8_t nibble) {
 		/* Increment pointer to the next pin */
 		p++;
 	}
-
+	/* Set enable pin */
 	gpio_set(*en);
+	/* Wait for 40 us */
 	sleep_us(40);
+	/* Clear enable pin */
 	gpio_clr(*en);
 }
 
+/*
+ * 	@brief	Write a new command in data pins
+ *
+ * 	@param	lcd: LCD pin struct
+ * 	@param	cmd: four bit command
+ *
+ * 	@return	None
+ */
 void lcd_put_command(lcd_t lcd, uint8_t cmd) {
-
+	/* Get pointer to rs pin */
 	pin_t *rs = lcd.rs;
-
+	/* Clear rs pin */
 	gpio_clr(*rs);
+	/* Put command in data pins */
 	lcd_put(lcd, cmd);
 }
 
+/*
+ * 	@brief	Write a new character in data pins
+ *
+ * 	@param	lcd: LCD pin struct
+ * 	@param	c: char to be sent
+ */
 void lcd_putc(lcd_t lcd, char c) {
-
+	/* Get pointer to rs pin */
 	pin_t *rs = lcd.rs;
-
+	/* Set rs pin */
 	gpio_set(*rs);
+	/* Repeat twice*/
 	for(uint8_t nibble = 0; nibble < 2; nibble++) {
+		/* Send lower nibble if it's the first time. Send higher nibble if second */
 		uint8_t n = (nibble)? c & 0x0f : c >> 4;
+		/* Write nibble */
 		lcd_put(lcd, n);
 	}
 }
 
+/*
+ * 	@brief	Write a new string in LCD
+ *
+ * 	@param	lcd: LCD pin struct
+ * 	@param	str: string to be sent
+ *
+ * 	@return None
+ */
 void lcd_puts(lcd_t lcd, const char* str) {
-
+	/* Loop intul null character */
 	while(*str) {
+		/* Write the char */
 		lcd_putc(lcd, *str);
+		/* Increment pointer */
 		str++;
 	}
 }
 
+/*
+ * 	@brief	Clear screen screen
+ *
+ * 	@oaram	lcd: LCD pin struct
+ *
+ * 	@return None
+ */
 void lcd_clear(lcd_t lcd) {
+	/* Send clear command:  first nibble */
 	lcd_put_command(lcd, 0x00);
+	/* Send clear command: second nibble */
 	lcd_put_command(lcd, 0x01);
+	/* Wait for 4ms */
 	sleep_ms(4);
 }
 
+/*
+ * 	@brief	Go to string coordinate
+ *
+ * 	@param	lcd: LCD pin struct
+ * 	@param	x: column number (starting in 0)
+ * 	@param	y: row number (string in 0)
+ */
 void lcd_go_to_xy(lcd_t lcd, uint8_t x, uint8_t y) {
-
+	/* Auxiliary variable */
 	uint8_t aux;
+	/* Check if the first row (0) is required */
 	if(y == 0) {
+		/* Set direction */
 		aux = 0x40 + x;
+		/* Send first nibble */
 		lcd_put_command(lcd, aux >> 4);
+		/* Send seconds byte */
 		lcd_put_command(lcd, aux & 0x0F);
 	}
+	/* Check if the second row (2) is required  */
 	else if (y == 1) {
+		/* Set direction */
 		aux = 0xC0 + x;;
+		/* Send first nibble */
 		lcd_put_command(lcd, aux >> 4);
+		/* Eend second nibble */
 		lcd_put_command(lcd, aux & 0x0F);
 	}
 }
