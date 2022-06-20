f449ac1e7eb6aa9017f4e346bb5797b328260661
Fabrizio Carlassara <53102151+carlassaraf@users.noreply.github.com>
2022-06-20 01:22:40 -0300

    add specific core delay function

--- api/ciaa_api_m0/inc/ciaa_stdlib_m0.h
+++ api/ciaa_api_m0/inc/ciaa_stdlib_m0.h
@@ -10,4 +10,22 @@
 
 #include "ciaa_stdlib.h"
 
+/*
+ * 	@brief	M0 sleep for a number of milliseconds
+ *
+ * 	@param	ms: number of milliseconds
+ *
+ * 	@return	None
+ */
+static inline void delay_ms(uint32_t ms) { sleep_ms(TIMER_3, ms); }
+
+/*
+ * 	@brief	M0 sleep for a number of microseconds
+ *
+ * 	@param	us: number of microseconds
+ *
+ * 	@return	None
+ */
+static inline void delay_us(uint32_t us) { sleep_us(TIMER_3, us); }
+
 #endif /* CIAA_STDLIB_M0_H_ */
--- api/ciaa_api_m4/inc/ciaa_stdlib_m4.h
+++ api/ciaa_api_m4/inc/ciaa_stdlib_m4.h
@@ -10,4 +10,22 @@
 
 #include "ciaa_stdlib.h"
 
+/*
+ * 	@brief	M0 sleep for a number of milliseconds
+ *
+ * 	@param	ms: number of milliseconds
+ *
+ * 	@return	None
+ */
+static inline void delay_ms(uint32_t ms) { sleep_ms(TIMER_0, ms); }
+
+/*
+ * 	@brief	M0 sleep for a number of microseconds
+ *
+ * 	@param	us: number of microseconds
+ *
+ * 	@return	None
+ */
+static inline void delay_us(uint32_t us) { sleep_us(TIMER_0, us); }
+
 #endif /* CIAA_STDLIB_M4_H_ */
