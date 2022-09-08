a5952ba9061b0a0a116ea06f8fa1a8e765da5dd9
Fabrizio <fabrizio.carlassara@gmail.com>
2022-09-08 19:13:33 -0300

    added board initialization

---
+++ api/ciaa_api/inc/ciaa_board_api.h
@@ -0,0 +1,15 @@
+/*
+ * ciaa_board_api.h
+ *
+ *  Created on: Sep 8, 2022
+ *      Author: carlassaraf
+ */
+
+#ifndef CIAA_BOARD_API_H_
+#define CIAA_BOARD_API_H_
+
+#include "ciaa_gpio_api.h"
+
+void ciaa_board_init(void);
+
+#endif /* CIAA_BOARD_API_H_ */
---
+++ api/ciaa_api/src/ciaa_board_api.c
@@ -0,0 +1,34 @@
+/*
+ * ciaa_board_api.c
+ *
+ *  Created on: Sep 8, 2022
+ *      Author: carlassaraf
+ */
+
+#include "ciaa_board_api.h"
+
+/*
+ *	@brief	Initializes common CIAA board GPIO
+ *
+ *	@param	None
+ *
+ *	@return	None
+ */
+void ciaa_board_init(void) {
+	/* Update system clock */
+    SystemCoreClockUpdate();
+    /* Initialize LEDs as output */
+    gpio_set_dir(LED1, true);
+    gpio_set_dir(LED2, true);
+    gpio_set_dir(LED3, true);
+    gpio_set_dir(LEDR, true);
+    gpio_set_dir(LEDB, true);
+    gpio_set_dir(LEDG, true);
+    /* Turn off LEDs */
+    gpio_clr(LED1);
+    gpio_clr(LED2);
+    gpio_clr(LED3);
+    gpio_clr(LEDR);
+    gpio_clr(LEDB);
+    gpio_clr(LEDG);
+}
