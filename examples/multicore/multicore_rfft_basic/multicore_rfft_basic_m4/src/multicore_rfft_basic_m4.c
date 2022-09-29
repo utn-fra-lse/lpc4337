dcacd276a670fb9afc39d79574f63f3d6fd8a60a
Fabrizio <fabrizio.carlassara@gmail.com>
2022-09-28 22:07:17 -0300

    example compiles, need to check result

--- examples/multicore/multicore_rfft_basic/multicore_rfft_basic_m4/src/multicore_rfft_basic_m4.c
+++ examples/multicore/multicore_rfft_basic/multicore_rfft_basic_m4/src/multicore_rfft_basic_m4.c
@@ -11,63 +11,45 @@
 #include "arm_math.h"
 #include "arm_const_structs.h"
 
-// using a 1024 point signal
-#define FFT_SAMPLES 32
-#define FFT_SAMPLES_HALF (FFT_SAMPLES / 2)
+/* Number of samples */
+#define FFT_SAMPLES 64
 
-float32_t testInput_f32_10khz[FFT_SAMPLES] = {
-
-	0.00000000,  2.01298520,  3.94355855,  5.71268215,
-	7.24792787,  8.48644257,  9.37752132,  9.88468324,
-	9.98716507,  9.68077119,  8.97804540,  7.90775737,
-	6.51372483,  4.85301963,  2.99363123,  1.01168322,
-   -1.01168322, -2.99363123, -4.85301963, -6.51372483,
-   -7.90775737, -8.97804540, -9.68077119, -9.98716507,
-   -9.88468324, -9.37752132, -8.48644257, -7.24792787,
-   -5.71268215, -3.94355855, -2.01298520,  0.00000000
+float32_t inputSignal[FFT_SAMPLES] = {
+	0.00000000e+00,  9.95678466e-02,  1.98146143e-01,  2.94755174e-01,
+	3.88434796e-01,  4.78253979e-01,  5.63320058e-01,  6.42787610e-01,
+	7.15866849e-01,  7.81831482e-01,  8.40025923e-01,  8.89871809e-01,
+	9.30873749e-01,  9.62624247e-01,  9.84807753e-01,  9.97203797e-01,
+	9.99689182e-01,  9.92239207e-01,  9.74927912e-01,  9.47927346e-01,
+	9.11505852e-01,  8.66025404e-01,  8.11938006e-01,  7.49781203e-01,
+	6.80172738e-01,  6.03804410e-01,  5.21435203e-01,  4.33883739e-01,
+	3.42020143e-01,  2.46757398e-01,  1.49042266e-01,  4.98458857e-02,
+	-4.98458857e-02, -1.49042266e-01, -2.46757398e-01, -3.42020143e-01,
+	-4.33883739e-01, -5.21435203e-01, -6.03804410e-01, -6.80172738e-01,
+	-7.49781203e-01, -8.11938006e-01, -8.66025404e-01, -9.11505852e-01,
+	-9.47927346e-01, -9.74927912e-01, -9.92239207e-01, -9.99689182e-01,
+	-9.97203797e-01, -9.84807753e-01, -9.62624247e-01, -9.30873749e-01,
+	-8.89871809e-01, -8.40025923e-01, -7.81831482e-01, -7.15866849e-01,
+	-6.42787610e-01, -5.63320058e-01, -4.78253979e-01, -3.88434796e-01,
+	-2.94755174e-01, -1.98146143e-01, -9.95678466e-02, -2.44929360e-16
 };
 
-#define inputSignal testInput_f32_10khz
-// see gist
-static float32_t complexFFT[FFT_SAMPLES], realFFT[FFT_SAMPLES_HALF],
-		imagFFT[FFT_SAMPLES_HALF], angleFFT[FFT_SAMPLES_HALF],
-		powerFFT[FFT_SAMPLES_HALF];
-
-uint32_t fftSize = FFT_SAMPLES;
-uint32_t ifftFlag = 0;
+/* RFFT instance, needs to be global */
 arm_rfft_fast_instance_f32 S;
-uint32_t maxIndex = 0;
-arm_status status;
-float32_t maxValue;
-int i;
 
 int main(void) {
 
-	status = ARM_MATH_SUCCESS;
-	status = arm_rfft_fast_init_f32(&S, fftSize);
-	// input is real, output is interleaved real and complex
-	arm_rfft_fast_f32(&S, inputSignal, complexFFT, ifftFlag);
+	/* RFFT result goes here */
+	float32_t outputSignal[FFT_SAMPLES];
 
-	// first entry is all real DC offset
-	//float32_t DCoffset = complexFFT[0];
+	/* Initialie RFFT instance */
+	arm_status status = arm_rfft_fast_init_f32(&S, FFT_SAMPLES);
 
-	// de-interleave real and complex values
-	for (i = 0; i < (FFT_SAMPLES / 2) - 1; i++) {
-		realFFT[i] = complexFFT[i * 2];
-		imagFFT[i] = complexFFT[(i * 2) + 1];
+	if(status == ARM_MATH_SUCCESS) {
+		/* Continue if initialization was a success */
+		arm_rfft_fast_f32(&S, inputSignal, outputSignal, 0);
+
+		while(1);
 	}
-
-  	  // find angle of FFT
-	for (i = 0; i < FFT_SAMPLES / 2; i++) {
-		angleFFT[i] = atan2f(imagFFT[i], realFFT[i]);
-	}
-	// compute power
-	arm_cmplx_mag_squared_f32(complexFFT, powerFFT, FFT_SAMPLES_HALF);
-	arm_max_f32(&powerFFT[1], FFT_SAMPLES_HALF - 1, &maxValue, &maxIndex);
-	// correct index
-	maxIndex += 1;
-
-	while(1);
 
 	return 0;
 }
