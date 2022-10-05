b7a3f590f1001fc53f84f7e4394c002693521fef
Fabrizio <fabrizio.carlassara@gmail.com>
2022-10-05 04:02:16 -0300

    sending data via usb

--- examples/multicore/multicore_adc_fft/multicore_adc_fft_m4/src/multicore_adc_fft_m4.c
+++ examples/multicore/multicore_adc_fft/multicore_adc_fft_m4/src/multicore_adc_fft_m4.c
@@ -10,24 +10,81 @@
 
 #include <stdio.h>
 #include "ciaa_ipc_api.h"
+#include "ciaa_usb_api.h"
+#include "arm_math.h"
+
+/* Number of ADC samples */
+#define N_SAMPLES	512
+
+/* Initialize ADC buffer */
+uint16_t adcResults[N_SAMPLES] = { 0 };
+
+/* Complex data */
+float32_t adcComplex[N_SAMPLES * 2] = { 0.0 };
+/* FFT output */
+float32_t fftOutput[N_SAMPLES];
+
+/* FFT configuration */
+uint32_t fftSize = N_SAMPLES / 2;
+uint32_t ifftFlag = 0;
+uint32_t doBitReverse = 1;
+/* CFFT instance */
+arm_cfft_instance_f32 s;
 
 int main(void) {
-	/* ADC result */
-	float result;
-	/* Conversion counter */
-	uint16_t i = 0;
+	/* Initialize USB */
+	usb_init();
 	/* IPC quque initialization */
-	ipc_queue_init(&result, sizeof(float), 1);
+	ipc_queue_init(adcResults, sizeof(uint16_t), N_SAMPLES);
+	/* ARM math status */
+	arm_status status = ARM_MATH_SUCCESS;
+	/* Initialize CFFT instance */
+	status = arm_cfft_init_f32(&s,fftSize);
     /* Start M0 core */
     multicore_m0_start();
 
 	while(1) {
 		/* Try to pop value from IPC */
-		if(ipc_try_pop(&result) == queue_valid) {
+		if(ipc_try_pop(adcResults) == queue_valid) {
+			/* Create complex array for FFT analysis */
+			for(uint16_t i = 0; i < N_SAMPLES; i++) {
+				/* Copy values in even indexes */
+				adcComplex[i * 2] = adcResults[i];
+			}
+			/* Process the data through the CFFT/CIFFT module */
+			arm_cfft_f32(&s, adcComplex, ifftFlag, doBitReverse);
+			/* Calculate the magnitude at each bin, it's symmetrical */
+			arm_cmplx_mag_f32(adcComplex, fftOutput, fftSize);
+			/* Continue if math was successful */
+			if (status == ARM_MATH_SUCCESS) {
+				/* Wait for host to be connected */
+				while(!usb_is_connected());
+				/* Get size of FFT result */
+				uint16_t n = sizeof(fftOutput) / sizeof(fftOutput[0]);
+				/* Send first couple of JSON format characters */
+				char str[25] = "{\"values\":[";
+				usb_send(str);
 #ifdef DEBUG
-    		/* Print result */
-    		printf("%04d: %3.2f\r\n", i++, result);
+				/* Print number of samples */
+				printf("FFT output with %d samples (symmetric values removed)\r\n", n / 2);
 #endif
+				/* Get rid of second half */
+				uint16_t i;
+				for(i = 0; i < n / 2; i++) {
+					/* Format string and send except the last one */
+					sprintf(str, "%e,", fftOutput[i]);
+					usb_send(str);
+#ifdef DEBUG
+					/* Print a new line every 16 values */
+					if(!(i % 16)) { printf("\r\n"); }
+					/* Print values */
+					printf("%12.6f,", i, fftOutput[i]);
+#endif
+					/* Send last value with closing JSON format characters */
+					sprintf(str, "%e]}", fftOutput[i]);
+					usb_send(str);
+				}
+			}
 		}
 	}
 	return 0;
