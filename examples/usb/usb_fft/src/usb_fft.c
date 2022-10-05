e948fdabd3fbd444f7395219f8cc12e6421fd0e2
Fabrizio <fabrizio.carlassara@gmail.com>
2022-10-05 00:05:21 -0300

    cfft to json working

--- examples/usb/usb_fft/src/usb_fft.c
+++ examples/usb/usb_fft/src/usb_fft.c
@@ -9,56 +9,54 @@
 */
 
 #include "ciaa_usb_api.h"
-#include "ciaa_stdlib.h"
 #include "arm_math.h"
-#include "arm_const_structs.h"
-#include <string.h>
 #include <stdio.h>
 
-/* Number of samples */
-#define FFT_SAMPLES 128
+#define N_SAMPLES 1024
 
-/* External test input */
-extern float32_t testInput_f32_1khz[128];
+/* Input to test */
+extern float32_t testInput[N_SAMPLES];
+/* Output buffer */
+static float32_t testOutput[N_SAMPLES / 2];
 
-/* RFFT instance, needs to be global */
-arm_rfft_fast_instance_f32 S;
+/* FFT configuration */
+uint32_t fftSize = N_SAMPLES / 2;
+uint32_t ifftFlag = 0;
+uint32_t doBitReverse = 1;
+/* CFFT instance */
+arm_cfft_instance_f32 s;
 
 int main(void) {
-	/* Update system clock */
-	SystemCoreClockUpdate();
 	/* Initialize USB0 with default configuration */
 	usb_init();
-	/* RFFT result goes here */
-	float32_t outputSignal[FFT_SAMPLES];
-	/* Initialize RFFT instance */
-	arm_status status = arm_rfft_fast_init_f32(&S, FFT_SAMPLES);
-
-	if(status == ARM_MATH_SUCCESS) {
-		/* Continue if initialization was a success */
-		arm_rfft_fast_f32(&S, testInput_f32_1khz, outputSignal, 0);
-
-		while(1) {
-			/* Check if host is connected */
-			if(usb_is_connected()) {
-				/* Send first couple of JSON format characters */
-				char str[25] = "{\"values\":[";
-				usb_send(str);
-				/* Convert every output value to string in scientific notation */
-				uint8_t i;
-				for(i = 0; i < FFT_SAMPLES - 1; i++) {
-					/* Format string and send except the last one */
-					sprintf(str, "%e,", outputSignal[i]);
-					usb_send(str);
-				}
-				/* Send last value with closing JSON format characters */
-				sprintf(str, "%e]}", outputSignal[i]);
-				usb_send(str);
-				/* Wait */
-				sleep_ms(2000);
-			}
+	/* ARM math status */
+	arm_status status = ARM_MATH_SUCCESS;
+	/* Initialize CFFT instance */
+	status = arm_cfft_init_f32(&s, fftSize);
+	/* Process the data through the CFFT/CIFFT module */
+	arm_cfft_f32(&s, testInput, ifftFlag, doBitReverse);
+	/* Calculate the magnitude at each bin, it's symmetrical */
+	arm_cmplx_mag_f32(testInput, testOutput, fftSize);
+	/* Continue if math was successful */
+	if (status == ARM_MATH_SUCCESS) {
+		/* Wait for host to be connected */
+		while(!usb_is_connected());
+		/* Get size of FFT result */
+		uint16_t n = sizeof(testOutput) / sizeof(testOutput[0]);
+		/* Send first couple of JSON format characters */
+		char str[25] = "{\"values\":[";
+		usb_send(str);
+		/* Convert every output value to string in scientific notation */
+		uint16_t i;
+		for(i = 0; i < n / 2 - 1; i++) {
+			/* Format string and send except the last one */
+			sprintf(str, "%e,", testOutput[i]);
+			usb_send(str);
 		}
-	}
+		/* Send last value with closing JSON format characters */
+		sprintf(str, "%e]}", testOutput[i]);
+		usb_send(str);
 
-	return 0;
+		while(1);
+	}
 }
