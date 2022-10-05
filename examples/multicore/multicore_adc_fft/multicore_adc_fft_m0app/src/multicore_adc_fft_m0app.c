f73b2e2809ea061b7c2e4bd2fe8326d154b50fe4
Fabrizio <fabrizio.carlassara@gmail.com>
2022-10-05 03:13:59 -0300

    init m0 project

---
+++ examples/multicore/multicore_adc_fft/multicore_adc_fft_m0app/src/multicore_adc_fft_m0app.c
@@ -0,0 +1,20 @@
+/*
+===============================================================================
+ Name        : multicore_adc_fft_m0app.c
+ Author      : $(author)
+ Version     :
+ Copyright   : $(copyright)
+ Description : main definition
+===============================================================================
+*/
+
+#include "ciaa_ipc_api.h"
+#include "ciaa_adc_api.h"
+
+int main(void) {
+
+	while(1) {
+
+	}
+	return 0;
+}
