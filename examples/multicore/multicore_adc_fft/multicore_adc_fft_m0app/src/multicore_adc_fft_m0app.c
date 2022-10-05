b41e215c70d77441cde19ccfc3ad2c1da241c85c
Fabrizio <fabrizio.carlassara@gmail.com>
2022-10-05 04:02:31 -0300

    converting 512 samples

--- examples/multicore/multicore_adc_fft/multicore_adc_fft_m0app/src/multicore_adc_fft_m0app.c
+++ examples/multicore/multicore_adc_fft/multicore_adc_fft_m0app/src/multicore_adc_fft_m0app.c
@@ -12,25 +12,36 @@
 #include "ciaa_adc_api.h"
 #include "ciaa_stdlib.h"
 
+/* Number of ADC samples */
+#define N_SAMPLES	512
+
+/* Initialize ADC buffer */
+uint16_t adcResults[N_SAMPLES] = { 0 };
+
 int main(void) {
-	/* ADC conversion factor */
-	const float conv_factor = 3.3 / (1 << 10);
 	/* ADC result */
 	float result;
+	/* Array index */
+	uint16_t index = 0;
 	/* ADC default initialization */
 	adc_init(0);
 	/* Select ADC channel 0 */
 	adc_select_input(0, ADC_CH0);
 	/* IPC quque initialization */
-	ipc_queue_init(&result, sizeof(float), 1);
+	ipc_queue_init(adcResults, sizeof(uint16_t), N_SAMPLES);
 
 	while(1) {
-		/* Do ADC conversion and get voltage */
-		result = conv_factor * adc_read(0);
-		/* Try to push value to IPC */
-		ipc_try_push(&result);
-		/* Wait half a second */
-		sleep_ms(500);
+		/* Do ADC conversion and store into results */
+		adcResults[index++] = adc_read(0);
+		/* Check if array is full already */
+		if(index == N_SAMPLES) {
+			/* Try to push samples to IPC */
+			ipc_try_push(adcResults);
+			/* Reset index */
+			index = 0;
+			/* Wait half a second */
+			sleep_ms(500);
+		}
 	}
 	return 0;
 }
