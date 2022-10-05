4c3d0d6b2dca863ffd670dfd7e140ea9281f7a9c
Fabrizio <fabrizio.carlassara@gmail.com>
2022-10-05 03:31:18 -0300

    adc values from m0 to m4

--- examples/multicore/multicore_adc_fft/multicore_adc_fft_m0app/src/multicore_adc_fft_m0app.c
+++ examples/multicore/multicore_adc_fft/multicore_adc_fft_m0app/src/multicore_adc_fft_m0app.c
@@ -10,11 +10,27 @@
 
 #include "ciaa_ipc_api.h"
 #include "ciaa_adc_api.h"
+#include "ciaa_stdlib.h"
 
 int main(void) {
+	/* ADC conversion factor */
+	const float conv_factor = 3.3 / (1 << 10);
+	/* ADC result */
+	float result;
+	/* ADC default initialization */
+	adc_init(0);
+	/* Select ADC channel 0 */
+	adc_select_input(0, ADC_CH0);
+	/* IPC quque initialization */
+	ipc_queue_init(&result, sizeof(float), 1);
 
 	while(1) {
-
+		/* Do ADC conversion and get voltage */
+		result = conv_factor * adc_read(0);
+		/* Try to push value to IPC */
+		ipc_try_push(&result);
+		/* Wait half a second */
+		sleep_ms(500);
 	}
 	return 0;
 }
--- examples/multicore/multicore_adc_fft/multicore_adc_fft_m4/src/multicore_adc_fft_m4.c
+++ examples/multicore/multicore_adc_fft/multicore_adc_fft_m4/src/multicore_adc_fft_m4.c
@@ -12,9 +12,23 @@
 #include "ciaa_ipc_api.h"
 
 int main(void) {
+	/* ADC result */
+	float result;
+	/* Conversion counter */
+	uint16_t i = 0;
+	/* IPC quque initialization */
+	ipc_queue_init(&result, sizeof(float), 1);
+    /* Start M0 core */
+    multicore_m0_start();
 
 	while(1) {
-
+		/* Try to pop value from IPC */
+		if(ipc_try_pop(&result) == queue_valid) {
+#ifdef DEBUG
+    		/* Print result */
+    		printf("%04d: %3.2f\r\n", i++, result);
+#endif
+		}
 	}
 	return 0;
 }
