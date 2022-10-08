8ee8776b9c1862cc423009d4c386d91e3764d3bd
Fabrizio <fabrizio.carlassara@gmail.com>
2022-10-08 00:56:36 -0300

    restore to previous working version

--- examples/multicore/multicore_adc_fft/multicore_adc_fft_m0app/src/multicore_adc_fft_m0app.c
+++ examples/multicore/multicore_adc_fft/multicore_adc_fft_m0app/src/multicore_adc_fft_m0app.c
@@ -16,40 +16,30 @@
 #define N_SAMPLES	2048
 
 int main(void) {
-	/* DC offset */
-	const float dc_offset = 1.2;
-	/* ADC conversion factor */
-	const float conv_factor = 3.3 / (1 << 10);
-	/* Initialize complex signal buffer */
-	float signal[N_SAMPLES * 2] = { 0.0 };
+	/* Initialize ADC buffer */
+	uint16_t adcResults[N_SAMPLES] = { 0 };
 	/* Results address */
-	uint32_t addr = (uint32_t)signal;
+	uint32_t addr = (uint32_t)adcResults;
 	/* Array index */
 	uint16_t index = 0;
-	/* Get ADC default configuration */
-	adc_config_t config = adc_get_default_config();
-	/* Change sampling frequency to 40KHz */
-	config.rate = 40000;
 	/* ADC default initialization */
-	adc_config_init(0, config);
+	adc_init(0);
 	/* Select ADC channel 0 */
 	adc_select_input(0, ADC_CH0);
 	/* IPC quque initialization */
 	ipc_queue_init(&addr, sizeof(uint32_t), 1);
 
 	while(1) {
-		/* Do ADC conversion and store into even indexes */
-		signal[index * 2] = conv_factor * adc_read(0) - dc_offset;
-		/* Clear imaginary indexes */
-		signal[(index * 2) + 1] = 0.0;
-		/* Increment index */
-		index++;
+		/* Do ADC conversion and store into results */
+		adcResults[index++] = adc_read(0);
 		/* Check if array is full already */
-		if(index == N_SAMPLES / 2) {
+		if(index == N_SAMPLES) {
 			/* Try to push samples address to IPC */
 			ipc_try_push(&addr);
 			/* Reset index */
 			index = 0;
+			/* Wait half a second */
+			sleep_ms(500);
 		}
 	}
 	return 0;
