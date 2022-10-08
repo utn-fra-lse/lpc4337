da09285131fbbfdfeea7f334612dfaec338bd27a
Fabrizio <fabrizio.carlassara@gmail.com>
2022-10-08 17:22:00 -0300

    update error values

--- pyusb/fft_error.py
+++ pyusb/fft_error.py
@@ -2,11 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 freq = [f for f in range(2500, 20000, 2500)]
-#err = [2812.5, 5703.125, 8515.625, 11328.125, 14218.75, 17031.25, 19843.75]
-err = [1406.25, 2851.5625, 4257.8125, 5564.0625, 7109.375, 8515.625, 9921.875]
+# Error measured for adc_fft example
+# err = [1406.25, 2851.5625, 4257.8125, 5564.0625, 7109.375, 8515.625, 9921.875]
+# Error measured for multicore_adc_fft example
+err = [1718.75, 3437.5, 5156.25, 6875, 8593.75, 10234.38, 12070.31]
 
+# Fit a linear function with those values
 f = np.polyfit(err, freq, 1)
 
+# Values to plot
 error = [er for er in range(0, 20000)]
 frequency = np.polyval(f, error)
 
