e161f430b7d9948ba85284af76625b232de8d6a2
Fabrizio <fabrizio.carlassara@gmail.com>
2022-10-17 01:20:58 -0300

    m0 example done

--- examples/freertos/freertos_multicore_blinky/freertos_multicore_blinky_m0app/src/freertos_multicore_blinky_m0app.c
+++ examples/freertos/freertos_multicore_blinky/freertos_multicore_blinky_m0app/src/freertos_multicore_blinky_m0app.c
@@ -12,29 +12,41 @@
 #include "task.h"
 #include "ciaa_gpio_api.h"
 
-void task_led2(void *params) {
+/* Task priority */
+#define tskLED2_PRIORITY	tskIDLE_PRIORITY + 1UL
 
+/**
+ * @brief Task that blinks LED2 every 500 ms
+ * @param params: aditional parameters for the task
+ * @return None
+ */
+void task_led2(void *params) {
+	/* Delay in ms */
 	const uint16_t delay_ms = 500;
 
 	while(1) {
-
+		/* Toggle LED2 */
 		gpio_xor(LED2);
+		/* Block task for 500 ms */
 		vTaskDelay(delay_ms / portTICK_RATE_MS);
 	}
 }
 
+
 int main(void) {
-
-
+	/* Update system clock */
+	SystemCoreClockUpdate();
+	/* Task creation */
 	xTaskCreate(
-		task_led2,
-		(const char*) "LED2 task",
-		configMINIMAL_STACK_SIZE,
-		NULL,
-		tskIDLE_PRIORITY,
-		NULL
+		task_led2,					/* Callback function */
+		(const char*) "LED2 task",	/* Debugging info */
+		configMINIMAL_STACK_SIZE,	/* Stack size */
+		NULL,						/* No parameters */
+		tskLED2_PRIORITY,			/* Task priority */
+		NULL						/* No handler */
 	);
 
+	/* Start scheduler */
 	vTaskStartScheduler();
 
 	while(1);
