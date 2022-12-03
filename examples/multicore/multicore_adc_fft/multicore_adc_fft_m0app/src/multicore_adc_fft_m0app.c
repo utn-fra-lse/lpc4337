8138de7d60deeaeed28bf9db4e0f64710726aaa7
Fabrizio <fabrizio.carlassara@gmail.com>
2022-12-03 15:40:37 -0300

    add dma support and bit resolution change

--- examples/multicore/multicore_adc_fft/multicore_adc_fft_m0app/src/multicore_adc_fft_m0app.c
+++ examples/multicore/multicore_adc_fft/multicore_adc_fft_m0app/src/multicore_adc_fft_m0app.c
@@ -10,6 +10,7 @@
 
 #include "ciaa_ipc_api.h"
 #include "ciaa_adc_api.h"
+#include "ciaa_dma_api.h"
 #include "ciaa_stdlib.h"
 #include "ciaa_board_api.h"
 
@@ -24,17 +25,24 @@ float complexInput[2 * MAX_SAMPLES] = { 0.0 };
 bool adc_done = false;
 /* ADC result */
 uint16_t adc_result;
+/* DMA result buffer */
+uint32_t dma_buffer[MAX_SAMPLES];
+/* DMA channel */
+uint8_t dma_channel;
+/* DMA transfer done flag */
+bool dma_transfer_done = false;
 
 /**
  * @brief Struct to hold shared data between cores
  */
 typedef struct {
 	uint16_t size;		/* Size of real samples */
+	uint8_t nclocks;	/* Number of clocks that the conversion takes */
 	float *samples;		/* Pointer to samples */
 } data_t;
 
-/* ADC interrupt handler prototype */
-void adc_conversion_done(void);
+/* DMA channel interrupt handler */
+void dma_handler(void);
 
 
 int main(void) {
@@ -42,8 +50,6 @@ int main(void) {
 	ciaa_board_init();
 	/* ADC conversion factor */
 	const float conversion_factor = 3.3 / (1 << 10);
-	/* Index */
-	uint16_t i = 0;
 	/* Extra variable to store size temporally */
 	uint16_t fftSize = MIN_SAMPLES;
 	/* Shared data */
@@ -58,20 +64,42 @@ int main(void) {
 	adc_config_t config = adc_get_default_config();
 	/* Enable burst mode */
 	config.burstMode = ENABLE;
-	/* Enable interrupt */
-	config.interrupt = ENABLE;
-	/* Takes four clocks to finish conversion (1.6MS/s) */
-	config.bitsAccuracy = adc_3bits;
+	/* Configure to 3 bits of resolution. Takes four clocks to finish conversion (1.125MS/s) */
+	if(!gpio_get(TEC1)) { config.bitsAccuracy = adc_3bits; }
+	/* Configure to 5 bits of resolution. Takes six clocks to finish conversion (750KS/s) */
+	else if(!gpio_get(TEC2)) { config.bitsAccuracy = adc_5bits; }
+	/* Configure to 7 bits of resolution. Takes eight clocks to finish conversion (562.5KS/2)  */
+	else if(!gpio_get(TEC3)) { config.bitsAccuracy = adc_7bits; }
+	/* Configure to 9 bits of resolution. Takes ten clocks to finish conversion (450KS/s) */
+	else if(!gpio_get(TEC4)) { config.bitsAccuracy = adc_9bits; }
+	/* Configure to 10 bits of resolution. Takes eleven clocks to finish conversion (~400KS/s) */
+	else { config.bitsAccuracy = adc_10bits; }
+	/* Set number of clocks that conversion takes */
+	data.nclocks = adc_get_number_of_clocks(config.bitsAccuracy);
 	/* ADC initialization */
 	adc_config_init(0, config);
 	/* Select ADC channel 0 */
 	adc_select_input(0, ADC_CH0);
-	/* Set interrupt handler */
-	adc_set_irq_handler(0, adc_conversion_done);
 	/* Enable interrupt on channel 0 */
 	adc_set_irq_channel_enabled(0, ADC_CH0, true);
 	/* IPC quque initialization */
 	ipc_queue_init(&addr, sizeof(uint32_t), 1);
+	/* Enable DMA controller */
+	dma_init();
+	/* Get a DMA free channel */
+	dma_channel = dma_get_free_channel();
+	/* Create DMA config struct */
+	dma_config_t dma_config = {
+		dma_channel,							/* DMA channel to use */
+		conn_adc0,								/* Source of DMA is ADC0 */
+		(uint32_t) dma_buffer,					/* Destiny address */
+		fftSize,								/* Size of the transfer */
+		GPDMA_TRANSFERTYPE_P2M_CONTROLLER_DMA	/* Peripheral to memory */
+	};
+	/* Set DMA interrupt handler */
+	dma_set_channel_handler(dma_config.channel, dma_handler);
+	/* Initiate DMA transfer */
+	dma_transfer(dma_config);
 
 	while(1) {
 		/* Check TEC1 (32 samples) */
@@ -82,36 +110,34 @@ int main(void) {
 		else if(!gpio_get(TEC3)) { fftSize = 128; }
 		/* Check TEC4 (256 samples) */
 		else if(!gpio_get(TEC4)) { fftSize = 256; }
-		/* Check ADC conversion */
-		if(adc_done) {
-			/* Clear odd indexes */
-			complexInput[(i * 2) + 1] = 0.0;
-			/* Copy values in even indexes */
-			complexInput[i * 2] = adc_result * conversion_factor;
-			/* Reset flag */
-			adc_done = false;
-			/* Increment index */
-			i++;
-			/* Check if we reached the necessary samples */
-			if(i == data.size) {
-				/* Try to push samples address to IPC */
-				ipc_try_push(&addr);
-				/* Reset index */
-				i = 0;
-				/* Update size for next sampling */
-				data.size = fftSize;
+		/* Check DMA transfer */
+		if(dma_transfer_done) {
+			/* Go through every ADC sample */
+			for(uint32_t i = 0; i < dma_config.size; i++) {
+				/* Clear odd indexes */
+				complexInput[(i * 2) + 1] = 0.0;
+				/* Copy ADC result */
+				complexInput[i * 2] = (dma_buffer[i] >> 4 & 0x03FF) * conversion_factor;
 			}
+			/* Clear flag */
+			dma_transfer_done = false;
+			/* Send data to M4 core */
+			ipc_try_push(&addr);
+			/* Update DMA transfer size */
+			dma_config.size = fftSize;
+			/* Update FFT size */
+			data.size = fftSize;
+			/* Initiate a new DMA transfer */
+			dma_transfer(dma_config);
 		}
 	}
 	return 0;
 }
 
 /**
- * @brief ADC0 interrupt handler
+ * @brief DMA interrupt handler
  */
-void adc_conversion_done(void) {
-	/* Get ADC value (10 bits) */
-	adc_result = adc_read(0);
-	/* Conversion done */
-	adc_done = true;
+void dma_handler(void) {
+	/* Raise flag */
+	dma_transfer_done = true;
 }
