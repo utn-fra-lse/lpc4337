c427b07c5f73fe430b9ff765c9f3cf4ac3d3d9fd
Fabrizio <fabrizio.carlassara@gmail.com>
2023-02-10 00:39:48 -0300

    plot high frequency bin

--- pyusb/usb_adc_fft.py
+++ pyusb/usb_adc_fft.py
@@ -56,12 +56,14 @@ try:
         plt.figure("CIAA FFT")
         plt.grid(True)
         plt.title("Sample FFT")
-        plt.ylabel("Amplitude")
+        plt.ylabel("Amplitude [V]")
         plt.xlabel("Frequency [Hz]")
         plt.ylim([0, 1.7])
-        plt.xlim([0, fs / 4])
+        plt.xlim([0, 20e3])
         # Plot up to fs / 2 the FFT values
         plt.stem(fn, fft, linefmt="blue", markerfmt="none", label=f"{N} samples @ {fs:.2e} Hz ({resolution} bit resolution)")
+        # Plot max frequency bin
+        plt.stem(fn[index], peak, linefmt="blue", label=f"Max value of {peak:.2f} V @ {fn[index]:.2f} Hz")
         # Show label
         plt.legend()
         # Pause plot
